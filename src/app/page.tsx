bbf488f0c3d7e5c4b4388a2cf281d63dbb10a824
ctsymons <chandlertsymons@yahoo.com>
2025-08-23 15:20:12 -0400

    Remove overriding body background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <section className="flex items-center justify-center py-24">
-      <h1 className="text-3xl font-bold">Welcome to Chandlers</h1>
+      <div className="bg-black/70 p-4 rounded">
+        <h1 className="text-3xl font-bold text-white">Welcome to Chandlers</h1>
+      </div>
     </section>
   );
 }
